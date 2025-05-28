81b5bf4d65d0350688e949f20201e90a15b7cde0
Nolbag <1paul.nolan@gmail.com>
2025-05-28 23:07:13 +0100

    modified:   ProgramFlow/guessinggame.py added new game hilo

--- ProgramFlow/guessinggame.py
+++ ProgramFlow/guessinggame.py
@@ -1,23 +1,27 @@
 import random
 
-highest = 10
+highest = 1000
 answer = random.randint(1, highest)
 # answer = random.randint(1, 10) # This line generates a random number between 1 and 10
-
+print (answer)
+guess = 0 # guess is initialized to 0 so that the while loop can start
 print("Please guess a number between 1 and {}: ".format(highest))
-guess = int(input())
-if guess == answer:
-    print("You got it first time")
-else:
+while guess != answer:
+    guess = int(input())
+    if guess == 0:
+        print("You have chosen to quit the game")
+        print("The answer was {}".format(answer))
+    break
+    if guess == answer:
+        print("Well done, you guessed it")
+        break
+    else:
         if guess < answer:
             print("Please guess higher")
-        elif guess > answer:
+        else:  # guess must be greater than answer
             print("Please guess lower")
-        guess = int(input())
-        if guess == answer:
-            print("Well done, you guessed it")
-        else:
-            print("Sorry, you have not guessed correctly")
+        print("Well done, you guessed it")
+
 
 # if guess != answer:
 #     if guess < answer:
